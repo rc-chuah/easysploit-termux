c65e24a6fda405308f1aacc8c1c8be85688d3546
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2020-10-09 20:36:25 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,7 +13,7 @@ echo -e '\e[1;33m
     / ____/___ ________  __/ ___/____  / /___  (_) /_
    / __/ / __ `/ ___/ / / /\__ \/ __ \/ / __ \/ / __/
   / /___/ /_/ (__  ) /_/ /___/ / /_/ / / /_/ / / /_   v4.2
- /_____/\__,_/____/\__, //____/ .___/_/\____/_/\__/  (Linux)
+ /_____/\__,_/____/\__, //____/ .___/_/\____/_/\__/  (Termux)
                   /____/     /_/                        \e[1;34m
                                 
                                 Created by "KALI LINUX TRICKS"
@@ -30,7 +30,7 @@ echo -e '\e[1;33m
 
                                                              \e[1;32m
 
-  To execute EasySploit type anywhere in your terminal "easysploit".  \e[0m 
+  To execute EasySploit type anywhere in your Termux "easysploit".  \e[0m 
 '
 
 
